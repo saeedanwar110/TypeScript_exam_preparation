6d940ef65c0ecb866121cb935a593110f9dcb073
saeed anwar <saeedanwarsangi0@gmail.com>
2024-05-22 12:54:19 +0500

    Create codes for Exam_preparation.

---
+++ variable.ts
@@ -0,0 +1,69 @@
+//  not allow space in variable
+
+//let my name = "saeed anwar";
+
+// we can use ...  letters, numbers, $ and _ in variable
+
+//let myname = "saeed anwar"; 
+//let my1name = "saeed anwar";
+//let my$name = "saeed anwar";
+//let _myname = "saeed anwar";
+
+// numbers are not allowed as the first charter of variable.
+//let 1myname = "saeed anwar";
+
+
+//          types of variable
+ //Const , let , var
+
+ // can not be reassigned or update value with const
+
+         // const my_name = "saeed anwar";
+        //my_name ="saeed";
+// can not be re declared in the same scope.
+        //const my_name = "saeed anwar";
+
+        // must be initialized at the time of declaration.
+        //const my_name : string    =  "saeed anwar";
+
+
+
+
+        //can be reassigned or update value with let
+       
+        //let my_name = "saeed anwar";
+        //my_name = "saeed";
+        //console.log(my_name);
+
+        // can not be re declared in the same block scope.
+       // let my_name = "saeed anwar";
+
+       // can be declared without initialization ans its value can be assigned later.
+//let my_name: string 
+//my_name="saeed anwar";
+
+
+
+             // var is same 
+       
+              //var my_name = "saeed anwar";
+        //my_name = "saeed";
+        //console.log(my_name);
+// can be re declared within the same or outer scope without any error.
+              //var my_name = "saeed anwar";
+
+              //can be declared without initialization ans its value can be assigned later.
+         var my_name :string
+              my_name = "saeed anwar";
+              console.log(my_name);
+
+
+// note
+// Recommended for defining constants and value that are not mean ti change.   for Const
+// Preferred for vaeiables that need to be reassinged or value will change over time.   in Let 
+//It`s better to use "const" and "let" insead of "var" it has some problems that can make your code confusing.
+
+
+
+
+
